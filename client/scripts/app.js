be99e8650520db71f7005873d4d053543f02042a
MKS-Student <mks.student@hackreactor.com>
2015-03-19 12:31:03 -0500

    got most working

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -7,15 +7,6 @@
 var app =  {
 init:function () {
 app.fetch();
-for(var i = 0; i<app.returnData.length;i++){
-	var temp = app.returnData[i];
-	if (temp.objectId===app.lastcheck){break;}
-	app['rooms'][temp.roomname]= app['rooms'][temp.roomname]||[];
-	app['rooms'][temp.roomname].push([temp.username,temp.text,temp.createdAt]);
-	app['lastcheck']=app.returnData[0].objectId;
-
-	}
-
 
 
 },
@@ -24,9 +15,10 @@ for(var i = 0; i<app.returnData.length;i++){
 'rooms':{},
 'returnData':{ },
 'server': 'https://api.parse.com/1/classes/chatterbox',
+"roomdone":{},
 
 send:function(message){
-	return $.ajax({
+	 $.ajax({
   // This is the url you should use to communicate with the parse API server.
   url: app.server,
   type: 'POST',
@@ -50,6 +42,15 @@ fetch: function(){
 		contentType: 'application/json',
 		success: function (data) {
 			app['returnData'] = data.results;
+			for(var i = 0; i<app.returnData.length;i++){
+				var temp = app.returnData[i];
+				if (temp.objectId===app.lastcheck){break;}
+				app['rooms'][temp.roomname]= app['rooms'][temp.roomname]||[];
+				app['rooms'][temp.roomname].push([temp.username,temp.text,temp.createdAt]);
+				app['lastcheck']=app.returnData[0].objectId;
+
+			}
+			app.autoaddroom()
 			console.log('chatterbox: Message retrieved');
 		},
 		error: function (data) {
@@ -67,7 +68,7 @@ addMessage:function(message) {
 //           text: 'Never underestimate the power of the Schwartz!',
 //           roomname: 'lobby'
 //         };
-	var tweet = '<div data-lobby=' + message.roomname + '><p class="username" data-name ='+message.username+'>' + message.username + ':</p><p class="text">' + message.text + '</p></div>';
+	var tweet = '<div data-lobby="' + message.roomname + '" class ="tweet"><p class="username" data-name ='+message.username+'>' + message.username + ':</p><p class="text">' + message.text + '</p></div>';
 	$('#chats').append(tweet);
 
 	},
@@ -77,24 +78,65 @@ clearMessages:function(){
 	  	node.removeChild(node.firstChild);
 	  }
 	},
+newMessage:function(){
+  //get data from textbox
+  var data = document.getElementById('textbox').value ;
+  var user = document.getElementById('username').value ;
+  var message={
+  	username:user,
+  	text: data,
+  	roomname: app.currentroom
+  };
+  app.send(message);
+  app.init();
+  setTimeout(function(){app.showRoom(app.currentroom); }, 1000);
+  console.log(message);
+},
 addRoom:function(newRoom){
-  var room = '<div class="room" data-room=' + newRoom + '>' + newRoom + '</div>';
+  	var room = '<div class="room" data-room="' + newRoom + '">' + newRoom + '</div>';
 	$('#roomSelect').append(room);
+	
+},
+autoaddroom: function(){
+	for(var r in app.rooms){
+		if(!(r in app['roomdone'])){
+			app.addRoom(''+r);
+			console.log(r)
+			app['roomdone'][r]=0;
+		}
+	}
+},
+'currentroom':null,
+showRoom:function(room) {  
+  app.clearMessages();
+//add all messages with property 'room' to div id="chats"
+  for (var i = 0; i < app.rooms[room].length; i++) {
+  	//0=user, 1=message, 2=date
+  	var chatMessage = {username:app.rooms[room][i][0], text:app.rooms[room][i][1], roomname: app.rooms[room][i][2] }
+  	app.addMessage(chatMessage);
+  }
+  app.currentroom= room;
 },
 addFriend:function(){
 
 }
 };
-app.init();
 
 $(document).ready(function(){
-	console.log('works');	
-	setInterval(function(){app.init()},1000);
+	//event listener for room selection:
+  $('#roomSelect').on('click','.room', function(){
+    var room = $(this).data('room');
+  	// console.log(app.rooms[room]);
+	app.showRoom(room);
+   });
+  $('#submit').on('click', function(){
+  	app.newMessage();
+  	console.log("clicks")
+
+  })
+	app.init();
+	setInterval(function(){app.init()},20000);
 		var node;
 	
-	for (var room in app.rooms) {
-	node = '<button class="room">' + room + '</button>'
-	$('#roomSelect').append(node);	
-	}
 });
 
